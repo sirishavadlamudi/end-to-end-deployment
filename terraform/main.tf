ecadc7e8579834f31eafa36d8aea304c6715baa3
sirishavadlamudi <vsirisha028@gmail.com>
2025-11-12 14:28:45 +0530

    updated main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -46,11 +46,11 @@ resource "google_compute_instance" "app_vm" {
   EOF
 }
 
-# Output the instance name and public IP
+# Output values
 output "instance_name" {
   value = google_compute_instance.app_vm.name
 }
 
 output "instance_ip" {
-  value =
+  value = google_compute_instance.app_vm.network_interface[0].access_config[0].nat_ip
 }
